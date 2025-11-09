1668b8bbe1e8b6e81ba22e91656de62f8a468cbb
Sebas <sebaslg96@gmail.com>
2025-11-09 18:24:40 -0500

    terminada base inicial, iniciando primer  crud

---
+++ electron/database.js
@@ -0,0 +1,24 @@
+import path from "path";
+import sqlite3 from "sqlite3";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const dbPath = path.join(__dirname, "inventario.db");
+const db = new sqlite3.Database(dbPath);
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS inventario (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      ref_name TEXT,
+      sku TEXT,
+      status TEXT,
+      date_created TEXT,
+      date_modify TEXT
+    )
+  `);
+});
+
+export default db;
