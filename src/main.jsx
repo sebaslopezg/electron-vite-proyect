5f944b029a2dad72bddbf3590cb41df0ca4cefd4
Sebas <sebaslg96@gmail.com>
2025-11-17 18:05:49 -0500

    instegrando niceadmin

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './assets/niceStyle.css';
+import './assets/niceMain.js'
 
 import App from './App.jsx'
 
