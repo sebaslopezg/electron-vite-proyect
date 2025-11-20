6788256b18812322e3465397e8da3e885a560764
Sebas <sebaslg96@gmail.com>
2025-11-19 22:37:41 -0500

    terminado de adaptar niceadmin, se integró también react router y se dejo funcional la sidebar con la etiqueta Link y use location para resaltar los botones de la sidebar que esten activos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { Productos } from './components/Productos.jsx';
 
-import { Header } from './components/layout/Header';
+import Dashboard from './components/layout/Dashboard';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,28 +8,10 @@ function App() {
 
   return <>
 
-    <Header />
-
-    <aside id="sidebar" className="sidebar">
-        {/* Your sidebar content */}
-    </aside>
-
-    <main id="main" className="main">
-        {/* Your dashboard content goes here */}
-        <h1>Dashboard Content</h1>
-    </main>
-
-    <a
-      href="#"
-      className={`back-to-top d-flex align-items-center justify-content-center ${showBackToTop ? 'active' : ''}`}
-      onClick={scrollToTop}
-    >
-      <i className="bi bi-arrow-up-short"></i>
-    </a>
+    <Dashboard />
 
   </>
 
 }
-
 export default App;
 
---
+++ src/components/pages/DashboardPage.jsx
@@ -0,0 +1,5 @@
+export const DashboardPage = () => {
+    return <>
+    <h1>HI MY NAME IS...</h1>
+    </>;
+}
