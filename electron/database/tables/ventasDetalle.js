7f7706e300e3fc8d62694b3a0064f4ce725ae39b
lulo <lopez.gallego2611@gmail.com>
2026-04-13 18:37:46 -0500

    ventas encargos

--- electron/database/tables/ventasDetalle.js
+++ electron/database/tables/ventasDetalle.js
@@ -14,6 +14,7 @@ export function createVentasDetalleTable() {
       descuento REAL,
       subtotal REAL,
       total REAL,
+      is_encargo INTEGER,
       status INTEGER,
       date_created TEXT,
       FOREIGN KEY (maestro_id) REFERENCES ventasMaestro(id)
